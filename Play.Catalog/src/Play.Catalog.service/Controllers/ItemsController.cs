44cecd51830e896f7b6a4f57e65ff2c66ee93359
Nattapong <nattapong.ccwr@gmail.com>
2022-12-26 10:18:37 +0700

    from laptop

--- Play.Catalog/src/Play.Catalog.service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.service/Controllers/ItemsController.cs
@@ -11,12 +11,7 @@ namespace Play.Catalog.Service.Controllers
   [Route("items")]
   public class ItemController : ControllerBase
   {
-    private static readonly List<ItemDto> items = new()
-    {
-      new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
-      new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
-      new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow),
-    };
+    
 
     // GET /items
     [HttpGet]
